17cc8a12e5d9aee35fa349b9fb412c84e14e87b5
pcezars <grimmpc@gmail.com>
2023-10-16 13:27:08 -0300

    modified:   articles/ArticlesController.js 	new file:   views/admin/articles/edit.ejs 	modified:   views/admin/articles/index.ejs 	modified:   views/partials/homenavbar.ejs

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -56,4 +56,44 @@ router.post("/admin/articles/delete", (req, res) => {
     };
 });
 
+router.get("/admin/articles/edit/:id", (req, res) => {
+    var id = req.params.id;
+
+    if(isNaN(id)){
+        res.redirect("/admin/articles");
+    }
+
+    Article.findByPk(id).then(article => {
+        if(article != undefined){
+            Category.findAll().then(categories => {
+                res.render("./admin/articles/edit", {article: article, categories: categories});
+            })
+        }else{
+            res.redirect("/admin/articles");
+        }
+    }).catch(error => {
+        res.redirect("/admin/articles");
+    })
+})
+
+router.post("/admin/articles/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body
+    var category = req.body.category;
+
+    Article.update({
+        title: title,
+        body: body,
+        slug: slugify(title),
+        categoryId: category
+    }, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles");
+    })
+})
+
 module.exports = router;
