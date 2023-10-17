69d83334f8a1cdfb30796465b94ca30ce0755f01
pcezars <grimmpc@gmail.com>
2023-10-17 16:13:57 -0300

    modified:   index.js

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize('guiapress', 'root', 'password', {
+const connection = new Sequelize('guiapress', 'root', '*lqbSav5t*', {
     host: 'localhost',
     dialect: 'mysql',
     timezone: '-03:00'
