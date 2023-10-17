3a008e5824a4758b029b4ada73082319e56e8073
pcezars <grimmpc@gmail.com>
2023-10-17 13:40:51 -0300

    Adding .gitignore

--- database/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require("sequelize");
-
-const connection = new Sequelize('guiapress', 'root', '*lqbSav5t*', {
-    host: 'localhost',
-    dialect: 'mysql',
-    timezone: '-03:00'
-});
-
-module.exports = connection;
